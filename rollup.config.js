26dfa7551128f293f97ba3631fa13374038f949e
FAL <falworks.contact@gmail.com>
2019-10-14 20:17:33 +0900

    edit banner comment

--- rollup.config.js
+++ rollup.config.js
@@ -10,8 +10,11 @@ const year = "2019";
 const description = `* An extension for p5.js.`;
 
 const bannerComment = `/**
+ * ${moduleName}
+ *
 ${description}
  * GitHub repository: {@link https://github.com/fal-works/${moduleName}}
+ *
  * @module ${moduleName}
  * @copyright ${year} FAL
  * @author FAL <contact@fal-works.com>
